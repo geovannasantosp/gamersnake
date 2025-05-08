f3a444b377cc3afbc839816fea17b3a4802d9059
gee_santosp <geovanna.gps.veleiros@gmail.com>
2025-05-08 16:00:48 -0300

    att js

--- script.js
+++ script.js
@@ -31,5 +31,54 @@ const updateFoodPosition = () =>
 const handleGameOver = () => {
     clearInterval(setIntervalId);
     alert("Game Over!");
-    location.reload
+    location.reload();
+}
+
+const changeDirection = e =>{
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowDown" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowDown" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+}
+}
+
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset})));
+
+/* COMEÇAR O GAME - INIT GAME */
+const initGame = () => {
+    if (gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"`
+
+    /* QUANDO A COBRA SE ALIMENTA */
+    if (snakeX === foodX && snakeY === foodY){
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]);
+        score++;
+        highScore = score >= highScore ? score : highScore
+
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerHTML = `Score: ${score}`;
+        highScoreElement.innerHTML = `High Score: ${highScore}`;
+    }
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    for (let i = snakeBody.length - 1; i > 0; i-- ){
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
+        return gameOver = true;
+    }
 }
